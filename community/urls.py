9f70e3ac078f43b0f6006a6b3c9da85e0f2e582d
jesustr20 <jesus.tr61@gmail.com>
2021-10-16 21:49:45 -0500

    youtube-avances-mejorarVista

--- community/urls.py
+++ community/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from community.views import PortadaView
+from community.views import PortadaView, ChannelYTView
 
 urlpatterns = [
     path('', PortadaView.as_view(), name='inicio'),
+    # path('channelYoutube/', ChannelYoutubeView.as_view(),
+    #     name='channelYoutube.html')
+    path('channelYoutube/', ChannelYTView.as_view(), name='channelYoutube'),
 ]
--- community/views.py
+++ community/views.py
@@ -1,6 +1,9 @@
+import json
+from os import close
 from django.views.generic import TemplateView
-from community.models import EnlacesAPI, Community_info
+from django.shortcuts import render
 
+from community.models import EnlacesAPI, Community_info
 
 
 class PortadaView(TemplateView):
@@ -11,3 +14,14 @@ class PortadaView(TemplateView):
         context['title'] = 'Inicio'
         context['comment'] = Community_info.objects.all()
         return context
+
+
+class ChannelYTView(TemplateView):
+    template_name = 'channelYoutube.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context = {}
+        with open('community/channels.json', 'r') as f:
+            context['channel'] = json.load(f)
+            return context
