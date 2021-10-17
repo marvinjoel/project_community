2c182731daee8e6e04cc55731f789ea8169ca271
jesustr20 <jesus.tr61@gmail.com>
2021-10-16 21:56:45 -0500

    eliminado comentarios

--- community/urls.py
+++ community/urls.py
@@ -3,7 +3,5 @@ from community.views import PortadaView, ChannelYTView
 
 urlpatterns = [
     path('', PortadaView.as_view(), name='inicio'),
-    # path('channelYoutube/', ChannelYoutubeView.as_view(),
-    #     name='channelYoutube.html')
     path('channelYoutube/', ChannelYTView.as_view(), name='channelYoutube'),
 ]
