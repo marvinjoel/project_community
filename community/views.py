f61f57eb05c2da84b569c436ed467f0768ba6031
jesustr20 <jesus.tr61@gmail.com>
2021-11-07 03:14:32 -0500

    vista de enlaces

--- community/views.py
+++ community/views.py
@@ -1,6 +1,6 @@
+from re import template
 from django.views.generic import TemplateView
-from community.models import EnlacesAPI, Community_info
-
+from community.models import EnlacesAPI, Community_info, Link_Group
 
 
 class PortadaView(TemplateView):
@@ -10,4 +10,13 @@ class PortadaView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Inicio'
         context['comment'] = Community_info.objects.all()
+        return context
+
+
+class LinksGroupView(TemplateView):
+    template_name = 'linkgroups.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['context'] = Link_Group.objects.all()
         return context
